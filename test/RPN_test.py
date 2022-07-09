bc6a01dba0bddcd52b4082f4ab185e19e897bc61
Christopher Norman <christopherjohnnorman@gmail.com>
2022-07-09 13:50:03 +0100

    Reformat files and add unit tests

--- test/RPN_test.py
+++ test/RPN_test.py
@@ -1,47 +1,77 @@
 import unittest
-from src.main import RPN
+from src.main import *
 
 
 class RpnTesting(unittest.TestCase):
+    def test_single_value(self):
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("2")
+        self.assertEqual(2, rpn_calculator.result)
+
+    def test_decimal_input(self):
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("1.2 - 1")
+        self.assertEqual(rpn_calculator.result, None)
+
     def test_add(self):
-        abc = RPN("1 2 +")
-        abc.evaluate()
-        self.assertEqual(3, abc.result)
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("1 2 +")
+        self.assertEqual(3, rpn_calculator.result)
 
     def test_subtract(self):
-        abc = RPN("1 2 -")
-        abc.evaluate()
-        self.assertEqual(-1, abc.result)
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("1 2 -")
+        self.assertEqual(-1, rpn_calculator.result)
 
     def test_divide(self):
-        abc = RPN("5 2 /")
-        abc.evaluate()
-        self.assertEqual(2, abc.result)
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("5 2 /")
+        self.assertEqual(2, rpn_calculator.result)
 
     def test_multiply(self):
-        abc = RPN("5 6 *")
-        abc.evaluate()
-        self.assertEqual(30, abc.result)
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("5 6 *")
+        self.assertEqual(30, rpn_calculator.result)
 
     def test_modulo(self):
-        abc = RPN("5 3 %")
-        abc.evaluate()
-        self.assertEqual(2, abc.result)
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("5 3 %")
+        self.assertEqual(2, rpn_calculator.result)
+
+    def test_multiple_valid_operators(self):
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("5 5 + 4 - 2 /")
+        self.assertEqual(3, rpn_calculator.result)
 
     def test_alpha_char(self):
-        abc = RPN("a 2 +")
-        abc.evaluate()
-        self.assertRaises(ValueError)
-
-    def test_too_few_operators(self):
-        abc = RPN("1 + + -")
-        abc.evaluate()
-        self.assertRaises(IndexError)
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("a 2 +")
+        self.assertEqual(rpn_calculator.result, None)
 
     def test_too_many_operators(self):
-        abc = RPN("1 2 3 4 -")
-        abc.evaluate()
-        self.assertRaises(ValueError)
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("1 + + -")
+        self.assertEqual(rpn_calculator.result, None)
+
+    def test_too_few_operators(self):
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("1 2 3 4 -")
+        self.assertEqual(rpn_calculator.result, None)
+
+    def test_no_space(self):
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("53%")
+        self.assertEqual(rpn_calculator.result, None)
+
+    def test_double_space(self):
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("5  3 -")
+        self.assertEqual(rpn_calculator.result, 2)
+
+    def test_division_by_zero(self):
+        rpn_calculator = RPN()
+        rpn_calculator.evaluate("5 0 /")
+        self.assertEqual(rpn_calculator.result, None)
 
 
 if __name__ == '__main__':
